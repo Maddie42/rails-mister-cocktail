344bd81033fb8f6896b7f4f4910ecabb0f271ef1
Madeleine Triboy <triboy.m@gmail.com>
2017-02-17 17:47:17 +0100

    c'est fonctionnel mais pas tres beau

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,9 +1,9 @@
 class Ingredient < ApplicationRecord
-   CATEGORIES = ["lemon", "ice", "mint leaves", "vodka", "pineapple juice", "rhum", "milk", "coffee liquor"]
+   # CATEGORIES = ["lemon", "ice", "mint leaves", "vodka", "pineapple juice", "rhum", "milk", "coffee liquor"]
 
   has_many :doses
   has_many :cocktails, through: :doses
   validates :name, presence: true, uniqueness: true
-  validates :name, inclusion: { in: CATEGORIES,
-    message: "%{value} is not included in the list"}
+  # validates :name, inclusion: { in: CATEGORIES,
+  #   message: "%{value} is not included in the list"}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  root to: 'cocktails#index'
+  mount Attachinary::Engine => "/attachinary"
 
-  resources :cocktails, only: [ :index, :show, :new, :create, :edit, :update] do
-    resources :doses, only: [ :new, :create]
+  resources :cocktails, only: [:show, :index, :new, :create] do
+    resources :doses, only: [:create]
   end
   resources :doses, only: [:destroy]
+  root "cocktails#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
